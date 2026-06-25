3e7b3a4ed96da22f98255999c5e89c16a4b6d78c
TyrEamon <yuanbin200409@gmail.com>
2026-06-25 17:02:49 +0800

    Update site icon assets

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,14 @@ export default function Document() {
           defaultColorScheme="auto"
           localStorageKey={MANTINE_COLOR_SCHEME_STORAGE_KEY}
         />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body>
         <Main />
